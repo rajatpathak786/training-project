fe9d1195966feb5c31d2b8bfc85248de38ec00a2
Rajat Pathak <rajatpathak786@gmail.com>
2019-04-28 18:43:20 +0530

    config file added

--- config/app.js
+++ config/app.js
@@ -1,11 +1,12 @@
 const convict = require('convict')
+const validate = require('validate.js')
 
 const config = convict({
   app: {
     name: {
-      doc: 'Patientory File Manager',
+      doc: 'Training Application',
       format: String,
-      default: 'Patientory File Server'
+      default: 'Training Application'
     }
   },
   env: {
@@ -17,10 +18,63 @@ const config = convict({
   port: {
     doc: 'The port to bind.',
     format: 'port',
-    default: 3001,
+    default: 4563,
     env: 'PORT'
   },
+  log_level: {
+    doc: 'level of logs to show',
+    format: String,
+    default: '',
+    env: 'LOG_LEVEL'
+  },
+  ip: {
+    doc: "The IP address to bind.",
+    format: "ipaddress",
+    default: "127.0.0.1",
+    env: "IP_ADDRESS"
+  },
+  db: {
+    host: {
+      default: "127.0.0.1",
+      env: "DB_HOST"
+    },
+    name : "mydatabase",
+    password: "123",
+    user: "postgres"
+  } 
 })
+/*log_level: {
+    'info': 0,
+    'ok': 1,
+    'error': 2
+  },*/
+/*const database = {
+  "development": {
+    "username": "postgres",
+    "password": "123",
+    "database": "mydatabase",
+    "host": "127.0.0.1",
+    "port": "5432",	
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": "123",
+    "database": "mydatabase",
+    "host": "127.0.0.1",
+    "port": "5432",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "postgres",
+    "password": "123",
+    "database": "mydatabase",
+    "host": "127.0.0.1",
+    "port": "5432",
+    "dialect": "postgres"
+  }
+} */
+
 
 config.validate({ allowed: 'strict' })
 

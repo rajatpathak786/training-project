9c3ef499b40af9d93d81827286646407dab94726
Rajat Pathak <rajatpathak786@gmail.com>
2019-05-01 16:17:11 +0530

    controllers added

--- app/controllers/sendEmail.js
+++ app/controllers/sendEmail.js
@@ -1,14 +1,26 @@
 import Responder from '../../server/expressResponder'
 import sendEmail from '../services/Email/sendEmail'
 import sendEmailParams from '../services/Email/sendEmailParams'
+import trelloUpdateList from '../services/Trello/trelloUpdateList'
+import trelloUpdateCard from '../services/Trello/trelloUpdateCard'
 
 export default class sendMail {
 
   static async sendEmail(req, res) {
-    const variable = req.body;
+    const variable = 'mail send & trello updated';
     const sendEmailResult = await sendEmail.execute(variable)
     if (sendEmailResult.successful) {
-      Responder.success(res, { variable })
+      const trelloUpdateListResult = await trelloUpdateList.execute(variable)
+        if(trelloUpdateListResult.successful) {
+          const trelloUpdateCardResult = await trelloUpdateCard.execute(variable)
+            if(trelloUpdateCardResult.successful) {
+              Responder.success(res, { variable })        
+            } else {
+              Responder.operationFailed(res, trelloUpdateCardResult.error)
+            }
+        } else {
+          Responder.operationFailed(res, trelloUpdateListResult.error)
+        }
     } else {
       Responder.operationFailed(res, sendEmailResult.error)
     }

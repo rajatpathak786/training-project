d353a324cef51a9d0f1c79574cbfcf6fc5bab593
Rajat Pathak <rajatpathak786@gmail.com>
2019-05-03 21:39:45 +0530

    test file and console log removed

--- app/services/Email/sendEmail.js
+++ app/services/Email/sendEmail.js
@@ -23,9 +23,7 @@ export default class sendEmail extends ServiceBase {
     try {
       let obj = {};
 
-      let sendEmail = async (obj) => {
-        console.log(obj);
-        console.log('14 14 14 14 14 14 14 14 14 14 14 14')
+      let sendEmail = async (obj) => {        
         let testAccount = await nodemailer.createTestAccount();
         let transporter = nodemailer.createTransport({
           host: "smtp.ethereal.email",
@@ -47,14 +45,12 @@ export default class sendEmail extends ServiceBase {
       }
 
       let assignmentMail = async (id) => {
-        console.log('888888888888888888          '+id)
         await emptrainingtable.findAll({
           attributes: ['reviewerId', 'empId', 'moduleId', 'taskId', 'expectedDateOfCompletion', 'taskStatus'],
           where: {id: id}
         })
         .then(async(trainingInfo) => {
           if (trainingInfo[0].taskStatus == 'not assigned') { 
-          console.log('9999999999999999999                '+trainingInfo[0]);
           await emptrainingtable.update(
             {taskStatus: 'mail send'},
             {where: {id: id}}
@@ -65,25 +61,21 @@ export default class sendEmail extends ServiceBase {
               where: {id: trainingInfo[0].empId}
             })
             .then(async(employeeDetails) => {
-              console.log('10 10 10 10 10 10 10 10 10 10 10 10       '+employeeDetails[0]);
               await emptable.findAll({
                 attributes: ['empEmail'],
                 where: {id: trainingInfo[0].reviewerId}
               })
               .then(async(reviewerEmail) => {
-                console.log('11 11 11 11 11 11 11 11 11 11        '+reviewerEmail[0]);
                 await moduletable.findAll({
                   attributes: ['moduleName'],
                   where: {id: trainingInfo[0].moduleId}
                 })
                 .then(async(moduleName) => {
-                  console.log('12 12 12 12 12 12 12 12 12     '+moduleName[0]);
                   await tasktable.findAll({
                     attributes: ['taskName'],
                     where: {id: trainingInfo[0].taskId}
                   })
                   .then(async(taskName) => {
-                    console.log('13 13 13 13 13 13 13 13 13 13 13        '+taskName[0]);
                     obj.recEmail = employeeDetails[0].empEmail;
                     obj.senEmail = reviewerEmail[0].empEmail;
                     obj.subject = `Complete ${moduleName[0].moduleName} ${taskName[0].taskName}`;
@@ -103,7 +95,6 @@ export default class sendEmail extends ServiceBase {
       
       let nextmail = schedule.scheduleJob('* /11 * * *', async function() {
         let date = Date.now();
-        console.log('111111111111     '+date);
         let formatDate = async (date) => {
           let currentDate = new Date(date),
           month = '' + (currentDate.getMonth() + 1),
@@ -114,26 +105,14 @@ export default class sendEmail extends ServiceBase {
           return [year, month, day].join('-');
         }
         date = await formatDate(date);
-        console.log('222222222222     '+date);
-        console.log('333333333333333');
-
-        console.log('4444444444444444444444444         '+date);
         await emptrainingtable.findAll({
           attributes: ['id'],
           where: {dateOfStart: date}
         })
         .then(async (empids) => {
-          console.dir(empids[0]);
-          console.log(typeof empids[0]);
-          console.log('5555555555555555555            '+empids[0].id+'      ');
           if(typeof empids[0] == 'undefined') {
-            console.log('No new assignments today');
           } else {
             let length = empids.length;
-            console.log(empids[0].id);
-            console.log(typeof empids[0].id);
-            console.log('6666666666666666666            '+length);
-            console.log('7777777777777777777777');
             for (let i = 0 ; i < length; ) {
               await assignmentMail(empids[i].id)
               console.log('after mail send');
